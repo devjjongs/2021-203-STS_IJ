c898924bd132f2dc9035e733ce522262862dddbe
devjjongs <whdtjr5689@gmail.com>
2021-07-28 11:39:45 +0900

    add & modified & deleted files

---
+++ src/main/java/spring_test/controller/HelloController.java
@@ -0,0 +1,28 @@
+package spring_test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+@Controller
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model){
+        logger.info("Welcome home! The client locale is {}", locale);
+
+        LocalDate localDate = LocalDate.now();
+
+        model.addAttribute("data",localDate);
+        return "home";
+    }
+
+}
